ef0a2c4eee5a56b353d890d045cd1075f3d30aa7
Svampex <adi_rad_96@hotmail.com>
2016-02-08 17:48:26 +0100

    Updated

--- src/MVC/Address.java
+++ src/MVC/Address.java
@@ -4,8 +4,9 @@ import java.util.regex.*;
 
 public class Address {
     private final String street, house, floor, side, postcode, city;
-    private static Pattern addressPattern = Pattern.compile("(?<vejnavn>[A-ZÆØÅa-zæøå ]+)(?<husnr>[1-9][0-9]*[A-Z]?)((?<etage> [1-9][0-9]*\\.[ ]?)(?<side>MF|TV|TH|mf|tv|th))?[,]? (?<postnr>[1-9][0-9]{3}) (?<bynavn>[A-ZÆØÅa-zæøå ]+)");
-
+    //pattern med: Vejnavn, husnr, etage (i form af tal), retning, postnr. og by.
+    private static Pattern addressPattern = Pattern.compile("(?<vejnavn>[A-ZÆØÅa-zæøå\\- ]+) (?<husnr>[1-9][0-9]*[A-Za-z]?)[,]?[ ]?((?<etage>[1-9][0-9]*\\.)[ ]?(?<side>MF|TV|TH|mf|tv|th))?[,]?[ ]?(?<postnr>[1-9][0-9]{3})?[ ]?(?<bynavn>[A-ZÆØÅa-zæøå ]+)?");
+    private static Pattern addressPattern2 = Pattern.compile("(?<vejnavn>[A-ZÆØÅa-zæøå ]+) (?<husnr>[1-9][0-9]*[A-Z]?)[,]?[ ]?((?<etage>st\\.?)[ ]?(?<side>([1-9][0-9]*)|((MF|TV|TH|mf|tv|th)\\.?))?)?[,]?[ ]?(?<postnr>[1-9][0-9]{3})?[ ]?(?<bynavn>[A-ZÆØÅa-zæøå ]+)?");
     private Address(String _street, String _house, String _floor, String _side, String _postcode, String _city) {
         street = _street;
         house = _house;
@@ -37,9 +38,13 @@ public class Address {
     public static Address parse(String s) {
         Builder b = new Builder();
         Matcher m = addressPattern.matcher(s);
+        Matcher m2 = addressPattern2.matcher(s);
         if(m.matches()){
             return b.street(m.group("vejnavn")).house(m.group("husnr")).floor(m.group("etage")).side(m.group("side")).postcode(m.group("postnr")).city(m.group("bynavn")).build();
         }
-        return null;
+        if(m2.matches()){
+            return b.street(m2.group("vejnavn")).house(m2.group("husnr")).floor(m2.group("etage")).side(m2.group("side")).postcode(m2.group("postnr")).city(m2.group("bynavn")).build();
+        }
+        return b.build();
     }
 }
